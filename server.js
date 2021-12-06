096839a250342c3f53dbd8c2f52d093a13f19510
dinumaan <dineshmatrix675@gmail.com>
2021-12-06 18:54:00 +0530

    added code for submisson for forms using

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ const express = require('express');
 
 const app = express();
 
+express.urlencoded({extended: true});
+
 app.get('/hello', (req, res)=>{
-    res.send("Hii");
+    let name = "Guest"
+    if(req.query.user){
+        name = req.query.user
+    }
+    res.send("Hello "+ name );
 })
 
 // we are mounting public folder on xyz route
 app.use('/xyz', express.static(__dirname + '/public')) // 
 
-
 app.listen(3000, () => {
     console.log("server started");
-})
+})  
